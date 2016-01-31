09deb1e13e1e7202adb8f02c8709a64460fbf8ac
amcaplan <ariel.caplan@vitals.com>
2016-01-31 20:24:26 +0200

    Add DynamicClass implementation

--- lib/dynamic_class.rb
+++ lib/dynamic_class.rb
@@ -1,5 +1,133 @@
 require "dynamic_class/version"
+require 'set'
 
 module DynamicClass
-  # Your code goes here...
+  def self.new(&block)
+    ::Class.new(::DynamicClass::Class).tap do |klass|
+      klass.class_exec(&block) if block_given?
+    end
+  end
+
+  class Class
+    class << self
+      def attributes
+        @attributes ||= Set.new
+      end
+
+      def inspect
+        super.insert(2, 'DynamicClass::')
+      end
+
+      # Always revert to original #to_h in case the parent class has already
+      # redefined #to_h.
+      def inherited(subclass)
+        subclass.class_eval <<-RUBY
+          def to_h
+            {}.tap do |hash|
+              each_pair do |key, value|
+                hash[key] = value
+              end
+            end
+          end
+        RUBY
+      end
+    end
+
+    def initialize(attributes = {})
+      attributes.each_pair do |key, value|
+        send(:[]=, key, value)
+      end
+    end
+
+    def to_h
+      {}.tap do |hash|
+        each_pair do |key, value|
+          hash[key] = value
+        end
+      end
+    end
+
+    def []=(key, value)
+      instance_variable_set(:"@#{key}", value)
+      key = key.to_sym
+      add_methods!(key) unless self.class.attributes.include?(key)
+    end
+
+    def [](key)
+      instance_variable_get(:"@#{key}")
+    end
+
+    def each_pair
+      return to_enum(__method__) { self.class.attributes.size } unless block_given?
+      self.class.attributes.map do |attribute|
+        yield(attribute, instance_variable_get(:"@#{attribute}"))
+      end
+    end
+
+    def method_missing(mid, *args)
+      len = args.length
+      if mname = mid[/.*(?==\z)/m]
+        if len != 1
+          raise ArgumentError, "wrong number of arguments (#{len} for 1)", caller(1)
+        end
+        self[mname] = args.first
+      elsif len == 0
+        self[mid]
+      else
+        raise ArgumentError, "wrong number of arguments (#{len} for 0)", caller(1)
+      end
+    end
+
+    def inspect
+      inspecting_ids = (Thread.current[:__inspecting_dynamic_class__] ||= [])
+      str = "#<DynamicClass::Class"
+      if inspecting_ids.include?(object_id)
+        return str << ' ...>'
+      else
+        begin
+          inspecting_ids.push(object_id)
+          attributes = self.class.attributes.map { |attribute|
+            "#{attribute}=#{self.send(attribute).inspect}"
+          }.join(', ')
+          return str << '>' if attributes.empty?
+          return str << ' ' << attributes << '>'
+        ensure
+          inspecting_ids.pop
+        end
+      end
+    end
+
+    def delete_field(key)
+      self[key] = nil
+    end
+
+    def ==(other)
+      other.is_a?(self.class) && to_h == other.to_h
+    end
+
+    def eql?(other)
+      other.is_a?(self.class) && to_h.eql?(other.to_h)
+    end
+
+    def hash
+      to_h.hash
+    end
+
+    private
+    def add_methods!(key)
+      self.class.send(:attr_accessor, key)
+      self.class.attributes << key
+      self.class.class_eval <<-RUBY
+        def to_h
+          {
+            #{
+              self.class.attributes.map { |attribute|
+                "#{attribute.inspect} => #{attribute}"
+              }.join(",\n")
+            }
+          }
+        end
+      RUBY
+    end
+  end
 end
--- spec/dynamic_class_spec.rb
+++ spec/dynamic_class_spec.rb
@@ -1,11 +1,296 @@
 require 'spec_helper'
+require 'ostruct'
 
 describe DynamicClass do
-  it 'has a version number' do
-    expect(DynamicClass::VERSION).not_to be nil
+  subject { klass.new(data) }
+
+  let(:klass) { described_class.new }
+  let(:data) {{}} # default, overridden in many tests
+
+  def expect_subject_responds(attribute)
+    expect(subject).to respond_to(attribute)
+    expect(subject).to respond_to(:"#{attribute}=")
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  def expect_subject_does_not_respond(attribute)
+    expect(subject).not_to respond_to(attribute)
+    expect(subject).not_to respond_to(:"#{attribute}=")
+  end
+
+
+  describe 'setting and getting values' do
+    context 'setting at initialization' do
+      let(:data) {{ foo: 'bar' }}
+
+      it 'retrieves via getter method' do
+        expect(subject.foo).to eq('bar')
+      end
+
+      it 'retrieves via [] method with Symbol' do
+        expect(subject[:foo]).to eq('bar')
+      end
+
+      it 'retrieves via [] method with String' do
+        expect(subject['foo']).to eq('bar')
+      end
+    end
+  end
+
+  describe 'append-only method signature' do
+   it 'does not respond to getters and setters by default' do
+      expect_subject_does_not_respond(:foo)
+    end
+
+    it 'appends getters and setters in response to setting a value at initialization' do
+      klass.new(foo: 'bar')
+      expect_subject_responds(:foo)
+    end
+
+    describe 'appending getters and setters in response to setting a value after initialization' do
+      it 'appends in response to a = method' do
+        subject.foo = 'bar'
+        expect_subject_responds(:foo)
+      end
+
+      it 'appends in response to the []= method with a Symbol' do
+        subject[:foo] = 'bar'
+        expect_subject_responds(:foo)
+      end
+
+      it 'appends in response to the []= method with a String' do
+        subject['foo'] = 'bar'
+        expect_subject_responds(:foo)
+      end
+    end
+
+    it 'does not append getters and setters in response to getting an attribute' do
+      subject.foo
+      expect_subject_does_not_respond(:foo)
+    end
+
+    it 'does not remove getters and setters when a field is deleted' do
+      subject.foo = 'bar'
+      subject.delete_field(:foo)
+      expect_subject_responds(:foo)
+    end
+  end
+
+  describe 'equality testing' do
+    let(:object1) { klass.new(data1) }
+    let(:object2) { klass.new(data2) }
+
+    context 'both objects are empty' do
+      let(:data1) {{}}
+      let(:data2) {{}}
+
+      it 'considers the objects equal' do
+        expect(object1).to eq(object2)
+      end
+    end
+
+    context 'one object is empty' do
+      let(:data1) {{}}
+      let(:data2) {{ a: 'foo' }}
+
+      it 'considers the objects unequal' do
+        expect(object1).not_to eq(object2)
+      end
+    end
+
+    context 'both objects hold the same data' do
+      let(:data1) {{ a: 'foo' }}
+      let(:data2) {{ a: 'foo' }}
+
+      it 'considers the objects equal' do
+        expect(object1).to eq(object2)
+      end
+    end
+
+    context 'both objects have the same keys but different values' do
+      let(:data1) {{ a: 'foo' }}
+      let(:data2) {{ a: 'bar' }}
+
+      it 'considers the objects unequal' do
+        expect(object1).not_to eq(object2)
+      end
+    end
+
+    context 'both objects have the same values but different keys' do
+      let(:data1) {{ a: 'foo' }}
+      let(:data2) {{ b: 'foo' }}
+
+      it 'considers the objects unequal' do
+        expect(object1).not_to eq(object2)
+      end
+    end
+  end
+
+  describe 'inputting and outputting hashes and hash-like objects' do
+    let(:data) {{name: "John Smith", age: 70, pension: 300}}
+
+    it 'outputs a hash equal to its input hash' do
+      expect(subject.to_h).to eq(data)
+    end
+
+    it "accepts another #{described_class} instance instead of a hash on initialization" do
+      expect(klass.new(subject).to_h).to eq(data)
+    end
+
+    it 'accepts a Struct instead of a hash on initialization' do
+      struct = Struct.new(*data.keys).new(*data.values)
+      expect(klass.new(struct).to_h).to eq(data)
+    end
+
+    it 'accepts an OpenStruct instead of a hash on initialization' do
+      open_struct = OpenStruct.new(data)
+      expect(klass.new(open_struct).to_h).to eq(data)
+    end
+  end
+
+  describe 'accessing keys and values with #each_pair' do
+    let(:data) {{ name: "John Smith", age: 70, pension: 300 }}
+
+    it 'iterates through each pair' do
+      expect(subject.each_pair.to_a).to eq(data.each_pair.to_a)
+    end
+  end
+
+  describe 'computing hash value' do
+    let(:data) {{ name: "John Smith", age: 70, pension: 300 }}
+
+    it 'uses the hash value of the internally built hash' do
+      expect(subject.hash).to eq(data.hash)
+    end
+  end
+
+  describe 'raising errors' do
+    context 'including an argument on a getter which has not yet been created' do
+      it 'raises an ArgumentError' do
+        expect { subject.foo(true) }.to raise_error(ArgumentError)
+      end
+    end
+
+    context 'including an extra argument on a setter which has not yet been created' do
+      it 'raises an ArgumentError' do
+        expect { subject.send(:foo=, 'bar', 'bar') }.to raise_error(ArgumentError)
+      end
+    end
+  end
+
+  describe 'relationship between instances of the same DynamicClass class' do
+    it 'makes the same methods available to other instances of the same class' do
+      expect_subject_does_not_respond(:foo)
+      klass.new(foo: 'bar')
+      expect_subject_responds(:foo)
+    end
+  end
+
+  describe 'relationship between instances of different DynamicClass classes' do
+    context 'the classes have no relationship' do
+      let(:klass2) { DynamicClass.new }
+
+      before do
+        klass2.new(foo: 'bar')
+        expect(klass2.new).to respond_to(:foo)
+      end
+
+      it 'does not impact an unrelated class' do
+        expect_subject_does_not_respond(:foo)
+      end
+    end
+
+    context 'one class inherits from the other' do
+      let(:klass2) { Class.new(klass) }
+      let(:child) { klass2.new }
+
+      context 'an attribute is added to the parent' do
+        before do
+          subject.foo = 'bar'
+          expect_subject_responds(:foo)
+        end
+
+        it 'inherits methods from a parent' do
+          expect(child).to respond_to(:foo)
+          expect(child).to respond_to(:foo=)
+          expect(subject.to_h).to have_key(:foo)
+        end
+
+        it 'does not include attributes from the parent in output hash' do
+          expect(child.to_h).not_to have_key(:foo)
+        end
+      end
+
+      context 'an attribute is added to the child' do
+        before do
+          child.foo = 'bar'
+          expect(child).to respond_to(:foo)
+          expect(child).to respond_to(:foo=)
+          expect(child.to_h).to have_key(:foo)
+        end
+
+        it 'does not pass methods up to the parent' do
+          expect_subject_does_not_respond(:foo)
+        end
+
+        it 'does not include attributes from the child in output hash' do
+          expect(subject.to_h).not_to have_key(:foo)
+        end
+      end
+    end
+  end
+
+  describe 'inspecting' do
+    context 'an empty instance' do
+      it 'shows it is an instance of a DynamicClass Class' do
+        expect(subject.inspect).to eq("#<DynamicClass::Class>")
+      end
+    end
+
+    context 'an instance with an assigned value' do
+      before do
+        subject.foo = 'bar'
+      end
+
+      it 'displays the value' do
+        expect(subject.inspect).to eq('#<DynamicClass::Class foo="bar">')
+      end
+    end
+
+    context 'an instance nested within an instance' do
+      before do
+        subject.foo = klass.new(foo: 'bar')
+      end
+
+      it 'displays the nested instance as a value' do
+        expect(subject.inspect).to eq('#<DynamicClass::Class foo=#<DynamicClass::Class foo="bar">>')
+      end
+    end
+
+    context 'an instance nested inside itself' do
+      before do
+        subject.foo = subject
+      end
+
+      it 'short-circuits inspection to avoid infinite recursion' do
+        expect(subject.inspect).to eq('#<DynamicClass::Class foo=#<DynamicClass::Class ...>>')
+      end
+    end
+  end
+
+  describe 'freezing' do
+    context 'subject is frozen' do
+      before do
+        subject.foo = 'bar'
+        subject.freeze
+      end
+
+      it 'raises a RuntimeError when adding a new value' do
+        expect { subject.baz = 'quux' }.to raise_error(RuntimeError)
+      end
+
+      it 'raises a RuntimeError when modifying a value' do
+        expect { subject.foo = 'quux' }.to raise_error(RuntimeError)
+      end
+    end
   end
 end
